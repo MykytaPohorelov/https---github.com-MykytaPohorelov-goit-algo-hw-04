a3338a0e8d7a49bda33657ca2b0bd056be8d9143
MykytaPohorelov <pogorelov.nikita95@gmail.com>
2024-05-02 13:41:44 +0200

    commit

--- main.py
+++ main.py
@@ -67,66 +67,66 @@ if cats_info is not None:
 
 # ------------
 
-# def parse_input(user_input):
-#     cmd, *args = user_input.split()
-#     cmd = cmd.strip().lower()
-#     return cmd, *args
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
-# def add_contact(args, contacts):
-#     name, phone = args
-#     contacts[name] = phone
-#     return "Contact added."
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
 
-# def change_contact(args, contacts):
-#     name, new_phone = args
-#     if name in contacts:
-#         contacts[name] = new_phone
-#         return f"Phone number for {name} updated."
-#     else:
-#         return f"{name} not found in contacts."
+def change_contact(args, contacts):
+    name, new_phone = args
+    if name in contacts:
+        contacts[name] = new_phone
+        return f"Phone number for {name} updated."
+    else:
+        return f"{name} not found in contacts."
     
-# def show_phone(args, contacts):
-#     name = args[0]
-#     if name in contacts:
-#         return f"Phone number for {name}: {contacts[name]}"
-#     else:
-#         return f"{name} not found in contacts."
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return f"Phone number for {name}: {contacts[name]}"
+    else:
+        return f"{name} not found in contacts."
     
-# def show_all(args, contacts):
-#     contact_list = []
-#     if contacts:
-#         result = "All contacts with phone numbers:\n"
-#         for name, phone in contacts.items():
-#             contact_list.append(contacts)
-#             # result += f"{name}: {phone}\n"
-#             result += f"{contact_list}"
-#             return result
-#     else:
-#         return "No contacts details."
+def show_all(args, contacts):
+    contact_list = []
+    if contacts:
+        result = "All contacts with phone numbers:\n"
+        for name, phone in contacts.items():
+            contact_list.append(contacts)
+            # result += f"{name}: {phone}\n"
+            result += f"{contact_list}"
+            return result
+    else:
+        return "No contacts details."
 
 
-# def main():
-#     contacts = {}
-#     print("Welcome to the assistant bot!")
-#     while True:
-#         user_input = input("Enter a command: ")
-#         command, *args = parse_input(user_input)
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
 
-#         if command in ["close", "exit"]:
-#             print("Good bye!")
-#             break
-#         elif command == "hello":
-#             print("How can I help you?")
-#         elif command == "add":
-#             print(add_contact(args, contacts))
-#         elif command == "change":
-#             print(change_contact(args, contacts))
-#         elif command == "phone":
-#             print(show_phone(args, contacts))
-#         elif command == "all":
-#             print(show_all(args, contacts))
-#         else:
-#             print("Invalid command.")
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(args, contacts))
+        else:
+            print("Invalid command.")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
